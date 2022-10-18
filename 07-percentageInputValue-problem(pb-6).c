ae1034be3fdebb63e9d83a431416b0c43dfdc030
bappasaha <45108418+bappasahabapi@users.noreply.github.com>
2022-10-18 17:18:06 +0600

    Add files via upload

---
+++ 07-percentageInputValue-problem(pb-6).c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    char c;
+    printf("Enter a number:\n " );
+    scanf("%d%c",&a,&c);
+   // scanf("%d%%",&a);
+    printf("%d",a);
+
+    return 0;
+
+}
+
