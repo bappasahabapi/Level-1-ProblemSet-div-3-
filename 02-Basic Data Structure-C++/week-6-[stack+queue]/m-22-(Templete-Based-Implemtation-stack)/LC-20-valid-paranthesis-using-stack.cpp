05a5d0ff25253b1918394a468c32ad1e4e9a6eb5
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2024-05-09 13:01:30 +0600

    Update Code

---
+++ 02-Basic Data Structure-C++/week-6-[stack+queue]/m-22-(Templete-Based-Implemtation-stack)/LC-20-valid-paranthesis-using-stack.cpp
@@ -0,0 +1,75 @@
+
+/*
+LeetCode-20 : Valid Bracket Sequence
+
+Steps:
+- Take user string input
+- Store every character in a variable
+- define string template class :  stack<char> st;
+
+Case:
+    1. if character start with valid bracket sequence
+            then push character to stack .
+        . else
+                it is empty
+                or, start and end with valid bracket sequence
+    3. if not valid sequence
+            then is it empty means valid
+            or simply invalid
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // user input:   ((){}[])
+
+    cout << "Enter Bracket Sequence:" << endl;
+    string str;
+    cin >> str;
+
+    stack<char> st;
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        char newChar = str[i];
+        // cout << newChar << endl;
+
+        if (newChar == '(' || newChar == '{' || newChar == '[')
+        {
+            st.push(newChar);
+        }
+        else
+        {
+            if (st.empty())
+            {
+                cout << "Invalid" << endl;
+                return 0;
+            }
+            else if(newChar == ')' && st.top() == '('){
+                    st.pop();
+            }
+            else if(newChar == '}' && st.top() == '{'){
+                    st.pop();
+            }
+            else if(newChar == ']' && st.top() == '['){
+                    st.pop();
+            }
+            else{
+                cout<<"Invalid\n";
+                return 0;
+            }
+        }
+    }
+    if (st.empty())
+    {
+        cout << "valid" << endl;
+    }
+    else
+    {
+        cout << "Invalid" << endl;
+    }
+
+    return 0;
+}
